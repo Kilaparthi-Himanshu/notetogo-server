a27cef3cc0748e4d6067794786cfa2172d9f3f68
Kilaparthi-Himanshu <himanshunani9@gmail.com>
2026-03-23 00:18:12 +0530

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,25 @@
+import { Server } from "@hocuspocus/server";
+
+const server = new Server({
+    port: 1234,
+});
+
+server.listen();
+
+console.log("Hocuspocus server running on ws://localhost:1234");
+
+// const server = new Server({
+//   port: 1234,
+
+//   onAuthenticate(data) {
+//     // auth logic (JWT, cookies etc.)
+//   },
+
+//   onConnect(data) {
+//     console.log("Client connected")
+//   },
+
+//   onDisconnect(data) {
+//     console.log("Client disconnected")
+//   },
+// })
