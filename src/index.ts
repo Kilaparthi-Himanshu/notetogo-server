1b1d495503b3f2ef55964910408068410fa73d26
Himanshu <himanshunani9@gmail.com>
2026-03-24 20:35:10 +0530

    Need to start work on adding supabase as the source of storage

--- src/index.ts
+++ src/index.ts
@@ -8,18 +8,18 @@ server.listen();
 
 console.log("Hocuspocus server running on ws://localhost:1234");
 
-// const server = new Server({
-//   port: 1234,
+    // const server = new Server({
+    //   port: 1234,
 
-//   onAuthenticate(data) {
-//     // auth logic (JWT, cookies etc.)
-//   },
+    //   onAuthenticate(data) {
+    //     // auth logic (JWT, cookies etc.)
+    //   },
 
-//   onConnect(data) {
-//     console.log("Client connected")
-//   },
+    //   onConnect(data) {
+    //     console.log("Client connected")
+    //   },
 
-//   onDisconnect(data) {
-//     console.log("Client disconnected")
-//   },
-// })
+    //   onDisconnect(data) {
+    //     console.log("Client disconnected")
+    //   },
+    // })
